b49078f74925e2bde0456d9dff8f31b71e73e643
Andrés Téllez <davidroa8@hotmail.com>
2025-04-12 23:13:18 -0500

    feat: add log receiver for queue messaging

---
+++ src/main/java/com/adt/logger/infrastructure/messaging/MessageReceiver.java
@@ -0,0 +1,18 @@
+package com.adt.logger.infrastructure.messaging;
+
+import com.adt.logger.application.events.emitters.LogEmitter;
+import com.adt.logger.domain.entities.Log;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MessageReceiver {
+    private final LogEmitter logEmitter;
+
+    @RabbitListener(queues = "${messaging.notifications-events.queue-name}")
+    public void log(Log log) {
+        logEmitter.emit(log);
+    }
+}
