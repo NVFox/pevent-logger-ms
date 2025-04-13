7bcb981bd30035bc40c65ea228443e9fcbe44a98
Andrés Téllez <davidroa8@hotmail.com>
2025-04-12 19:33:47 -0500

    feat: add logs service, with impl

---
+++ src/main/java/com/adt/logger/domain/services/LogService.java
@@ -0,0 +1,12 @@
+package com.adt.logger.domain.services;
+
+import com.adt.logger.domain.entities.Log;
+import com.adt.logger.domain.repositories.filters.LogFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import reactor.core.publisher.Mono;
+
+public interface LogService {
+    Mono<Log> create(Log log);
+    Mono<Page<Log>> findBy(LogFilter filter, Pageable pageable);
+}
