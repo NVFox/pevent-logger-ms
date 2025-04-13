1490d54793b8234261b9f6d3c885cf9505b5c71e
Andrés Téllez <davidroa8@hotmail.com>
2025-04-12 19:15:37 -0500

    feat: add log entity

---
+++ src/main/java/com/adt/logger/domain/entities/Log.java
@@ -0,0 +1,51 @@
+package com.adt.logger.domain.entities;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.index.TextIndexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "logs")
+@CompoundIndex(def = "{'level': 1, 'timestamp': -1}")
+@Data
+public class Log {
+    @Id
+    private String traceId;
+
+    @Indexed
+    private Level level;
+
+    @TextIndexed
+    private String message;
+
+    @Indexed(direction = IndexDirection.DESCENDING)
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+    public static Log info(String message) {
+        Log log = new Log();
+
+        log.setLevel(Level.INFO);
+        log.setMessage(message);
+
+        return log;
+    }
+
+    public static Log error(String message) {
+        Log log = new Log();
+
+        log.setLevel(Level.ERROR);
+        log.setMessage(message);
+
+        return log;
+    }
+
+    public enum Level {
+        INFO,
+        ERROR
+    }
+}
